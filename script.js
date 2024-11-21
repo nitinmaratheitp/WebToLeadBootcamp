90f3eecccbe0489d80d1392390e424936d9259a7
nitinmaratheitp <149466976+nitinmaratheitp@users.noreply.github.com>
2024-11-21 18:42:47 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function beforesubmit(){
+    let outputdate = document.querySelector(".outputdate");
+    let inputdate = document.querySelector(".inputdate");
+
+    console.log("inputdate.value", inputdate.value);
+
+    let formattedDate = new Date(inputdate.value).toLocaleDateString("en-US");
+    outputdate.value = formattedDate;
+}
